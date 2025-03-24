0f74a63628131554bc44df016a18afa5c9c1fc8b
mabohao <fengchuidujiangsheng@xinqijitech.com>
2025-03-24 11:01:10 +0800

    change logic

--- homowork1.py
+++ homowork1.py
@@ -8,11 +8,11 @@ matplotlib.rcParams['font.sans-serif'] = ['SimHei']  # 中文显示
 matplotlib.rcParams['axes.unicode_minus'] = False  # 解决负号 "-" 显示为方块的问题
 
 
-class InteractivePolyline3D:
+class InteractiveGrid3D:
     def __init__(self):
         self.fig = plt.figure()
         self.ax = self.fig.add_subplot(111, projection='3d')
-        self.ax.set_title("3D 交互折线绘制\n按Enter循环选择点\n方向键移动点，空格升高/Alt降低")
+        self.ax.set_title("3D 交互网格绘制\n按Enter循环选择点\n方向键移动点，空格升高/Alt降低")
 
         self.ax.set_xlim(-10, 10)
         self.ax.set_ylim(-10, 10)
@@ -23,14 +23,33 @@ class InteractivePolyline3D:
         self.move_speed = 0.5  # 移动速度
 
         # 初始化 4x4 点阵
-        self.points = np.array([[x, y, 0] for x in np.linspace(-5, 5, 4) for y in np.linspace(-5, 5, 4)])
+        self.grid_size = 4
+        x_vals = np.linspace(-5, 5, self.grid_size)
+        y_vals = np.linspace(-5, 5, self.grid_size)
+        self.points = np.array([[x, y, 0] for x in x_vals for y in y_vals])
 
-        # 初始化散点图和折线
-        self.scatter = self.ax.scatter(self.points[:, 0], self.points[:, 1], self.points[:, 2], 
-                                     c="red", s=50)
-        self.line, = self.ax.plot(self.points[:, 0], self.points[:, 1], self.points[:, 2], 
-                                 c="blue", linestyle="-", marker="o")
-        
+        # 初始化散点图
+        self.scatter = self.ax.scatter(self.points[:, 0], self.points[:, 1], self.points[:, 2],
+                                       c="red", s=50)
+
+        # 初始化网格线
+        self.lines = []
+        for i in range(self.grid_size):
+            # 横向连接每行点
+            row_indices = range(i * self.grid_size, (i + 1) * self.grid_size)
+            line, = self.ax.plot(self.points[row_indices, 0],
+                                 self.points[row_indices, 1],
+                                 self.points[row_indices, 2], c="blue")
+            self.lines.append(line)
+
+        for j in range(self.grid_size):
+            # 纵向连接每列点
+            col_indices = range(j, self.grid_size ** 2, self.grid_size)
+            line, = self.ax.plot(self.points[col_indices, 0],
+                                 self.points[col_indices, 1],
+                                 self.points[col_indices, 2], c="blue")
+            self.lines.append(line)
+
         # 高亮显示当前选中的点
         self.highlight_scatter = self.ax.scatter([], [], [], c="yellow", s=100)
 
@@ -46,61 +65,60 @@ class InteractivePolyline3D:
     def update_coord_text(self):
         """更新坐标显示文本"""
         point = self.points[self.current_point_index]
-        self.coord_text.set_text(f'当前点 #{self.current_point_index}\nX: {point[0]:.2f}\nY: {point[1]:.2f}\nZ: {point[2]:.2f}')
+        self.coord_text.set_text(
+            f'当前点 #{self.current_point_index}\nX: {point[0]:.2f}\nY: {point[1]:.2f}\nZ: {point[2]:.2f}')
 
     def on_key_press(self, event):
         """键盘事件处理"""
         if event.key == 'enter':
             # 循环选择下一个点
             self.current_point_index = (self.current_point_index + 1) % len(self.points)
-        
+
         elif event.key == 'left':
-            # 向左移动
             self.points[self.current_point_index][0] -= self.move_speed
         elif event.key == 'right':
-            # 向右移动
             self.points[self.current_point_index][0] += self.move_speed
         elif event.key == 'up':
-            # 向前移动
             self.points[self.current_point_index][1] += self.move_speed
         elif event.key == 'down':
-            # 向后移动
             self.points[self.current_point_index][1] -= self.move_speed
         elif event.key == ' ':  # 空格键
-            # 增加高度
             self.points[self.current_point_index][2] += self.move_speed
         elif event.key == 'alt':
-            # 降低高度
             self.points[self.current_point_index][2] -= self.move_speed
 
         # 限制坐标范围
         self.points[self.current_point_index] = np.clip(self.points[self.current_point_index], -10, 10)
-        
+
         # 更新图形
         self.update_plot()
 
     def update_plot(self):
-        """更新图形，重新绘制散点和折线"""
-        # 更新所有点
+        """更新图形，重新绘制散点和网格"""
         self.scatter._offsets3d = (self.points[:, 0], self.points[:, 1], self.points[:, 2])
-        self.line.set_data(self.points[:, 0], self.points[:, 1])
-        self.line.set_3d_properties(self.points[:, 2])
-        
-        # 更新高亮点
+
+        for i in range(self.grid_size):
+            row_indices = range(i * self.grid_size, (i + 1) * self.grid_size)
+            self.lines[i].set_data(self.points[row_indices, 0], self.points[row_indices, 1])
+            self.lines[i].set_3d_properties(self.points[row_indices, 2])
+
+        for j in range(self.grid_size):
+            col_indices = range(j, self.grid_size ** 2, self.grid_size)
+            self.lines[self.grid_size + j].set_data(self.points[col_indices, 0], self.points[col_indices, 1])
+            self.lines[self.grid_size + j].set_3d_properties(self.points[col_indices, 2])
+
         current_point = self.points[self.current_point_index]
         self.highlight_scatter._offsets3d = ([current_point[0]], [current_point[1]], [current_point[2]])
-        
-        # 更新坐标显示
+
         self.update_coord_text()
-        
+
         self.fig.canvas.draw_idle()
 
     def run(self):
-        # 初始化高亮显示第一个点和坐标
         self.update_plot()
         plt.show()
 
 
 if __name__ == "__main__":
-    app = InteractivePolyline3D()
+    app = InteractiveGrid3D()
     app.run()
